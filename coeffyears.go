97e5d8c7b775478cb604eb64be21fe7fc27f9712
Euan <euanmeston@gmail.com>
2023-08-12 22:25:48 +0100

    I'll fix the tests tomorrow

--- coeffyears.go
+++ coeffyears.go
@@ -1,8 +1,27 @@
 package sinclairgo
 
 const (
-	aMale2022   = 0.751945030
-	bMale2022   = 175.508
-	aFemale2022 = 0.783497476
-	bFemale2022 = 153.655
+	// 01-01-2009 - 31-12-2012 Coefficients
+	AMale2009   = 0.784780654
+	BMale2009   = 173.961
+	AFemale2009 = 1.056683941
+	BFemale2009 = 125.441
+
+	// 01-01-2013 - 31-12-2016 Coefficients
+	AMale2013   = 0.794358141
+	BMale2013   = 174.393
+	AFemale2013 = 0.897260740
+	BFemale2013 = 148.026
+
+	// 01-01-2017 - 31-12-2020 Coefficients
+	AMale20217  = 0.751945030
+	BMale2017   = 175.508
+	AFemale2017 = 0.783497476
+	BFemale2017 = 153.655
+
+	// 01-01-2021 - 31-12-2024 Coefficients
+	AMale2021   = 0.722762521
+	BMale2021   = 193.609
+	AFemale2021 = 0.787004341
+	BFemale2021 = 153.757
 )
